3709e788d9282154eeeac65913a0b4da3c7d7b5b
Yobbi <yohana.calbuyahue.a@gmail.com>
2022-09-09 18:34:15 -0400

    modifica html tabla students list getdata

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -13,10 +13,10 @@
         
         <div class="card-header">
             <div class="d-flex justify-content-between align-items-center">
-                <h2><i class="material-icons fs-1">people</i> Lista de Usuarios</h2>
+                <h2><i class="material-icons fs-1">people</i> Lista de Estudiantes</h2>
                 <a  href="{{ url('config/users/add')}}" class="btn btn-success">
                     <i class="material-icons">add</i>
-                    Agregar Usuario
+                    Agregar Estudiante
                 </a>
             </div>
         </div>
@@ -25,52 +25,16 @@
                 <thead>
                     <tr>
                         <th>Nombre</th>
+                        <th>Rut</th>
                         <th>E-mail</th>
-                        <th>Acción</th>
+                        <th>Telefono</th>
+                        <th>Fecha Nacimiento</th>
+                        <th>Opciones</th>
                     </tr>
                 </thead>
                 <tbody>
                 </tbody>
             </table>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="FormModal" tabindex="-1" aria-labelledby="FormModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="FormModalLabel">Usuario</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary">Guardar</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="DeleteModal" tabindex="-1" aria-labelledby="DeleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content bg-danger text-white">
-                <div class="modal-header justify-content-center">
-                    <h5 class="modal-title" id="DeleteModalLabel">
-                        <i class="material-icons">warning</i> Eliminar Usuario <i class="material-icons">warning</i>
-                    </h5>
-                </div>
-                <div class="modal-body">
-                    ¿Realmente desea eliminar a <span id="DeleteModalName"></span> de la lista de usuarios?
-                </div>
-                <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><i class="material-icons">close</i> Cancelar</button>
-                    <button type="button" class="btn btn-outline-light"><i class="material-icons">delete</i> Eliminar</button>
-                </div>
-            </div>
         </div>
     </div>
 
@@ -103,17 +67,18 @@
                 },     
                 "processing" : true,
                 "serverSide" : true,
-                "ajax" : "{{ url('config/users/getdata') }}",
+                "ajax" : "{{ url('student/getdata') }}",
                 "columns": [
-                    { "data": "name","width":"60%"},
-                    { "data": "email","width":"40%"},
+                    { "data": "name"},
+                    { "data": "rut"},
+                    { "data": "email"},
+                    { "data": "phone"},
+                    { "data": "birthday"},
                     { data: "id", render : function ( data, type, row, meta ) {
-                        var buttons = '<div class="btn-group" role="group" aria-label="Basic mixed styles example">';
-                        buttons+= '<a class="btn btn-primary" href="{{ url("config/users")}}/'+data+'" title="Editar"><i class="material-icons">edit</i></a>';
-                        buttons+= '<button type="button" class="btn btn-danger" onclick="deleteShow('+data+')" title="Eliminar"><i class="material-icons">delete</i></button>';
-                        buttons+= '</div>';
+                         var buttons = '<a class="btn btn-primary" href="{{ url("config/users")}}/'+data+'" title="Editar"><i class="material-icons">edit</i></a>';
                         return buttons;
-                    },"width":"1%"},
+                    }}
+                  
                 ],
                 language: lenguaje_es,
                 "order": [[ 0, "desc" ]]
