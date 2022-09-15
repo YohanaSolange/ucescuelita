c0d4c3b85165ca7435a68e43ffbf9fede4b2fbab
Yobbi <yohana.calbuyahue.a@gmail.com>
2022-09-15 16:11:58 -0300

    View list refenciar url student/add agregar estudiantes

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -14,7 +14,7 @@
         <div class="card-header">
             <div class="d-flex justify-content-between align-items-center">
                 <h2><i class="material-icons fs-1">people</i> Lista de Estudiantes</h2>
-                <a  href="{{ url('config/users/add')}}" class="btn btn-success">
+                <a  href="{{ url('student/add')}}" class="btn btn-success">
                     <i class="material-icons">add</i>
                     Agregar Estudiante
                 </a>
