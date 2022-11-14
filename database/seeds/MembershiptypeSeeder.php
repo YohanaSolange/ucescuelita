581339a3ad145d94d56b5f1595c2e98e1b11e50a
Dev 1 <dev1@gmail.com>
2022-11-14 18:48:00 -0300

    Añade membresias por defecto al estudiante para realizar pruebas

--- database/seeds/MembershiptypeSeeder.php
+++ database/seeds/MembershiptypeSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Membershiptype;
 
 class MembershiptypeSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class MembershiptypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $membership = Membershiptype::create(['name' => 'Mensualidad']);
+        $membership = Membershiptype::create(['name' => 'Matricula']);
+        $membership = Membershiptype::create(['name' => 'Otro']);
     }
 }
